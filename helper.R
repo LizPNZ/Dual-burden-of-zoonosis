0b4a60ae841e364efcb4a66cc76e2050e2e0714e
LizPNZ <56521009+LizPNZ@users.noreply.github.com>
2022-04-08 09:03:40 +0200

    Add files via upload

---
+++ helper.R
@@ -0,0 +1,18 @@
+
+
+## Helper function
+
+fit_gamma <-
+  function(lwr, upr) {
+    f <-
+      function(par, target) {
+        ## calculate sum of squared deviations
+        sum((qgamma(c(.025, .975), par[1], par[2]) - target) ^ 2)
+      } 
+    
+    optim(c(1, 1), f, target = c(lwr, upr))
+  }
+
+### Source of fit_gamma function: https://github.com/brechtdv/tsol-mozambique
+
+
